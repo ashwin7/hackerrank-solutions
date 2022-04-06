be12dde9d0f0da2648e161aa1149621b476b6411
Ashwin Saxena <ashwinsaxena7@gmail.com>
2022-04-06 16:21:29 +0530

    jumping-on-the-clouds

---
+++ jumping-on-the-clouds/jumping-on-the-clouds.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'jumpingOnClouds' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY c as parameter.
+ */
+
+function jumpingOnClouds(c) {
+    // Write your code here
+    let jumps = 0;
+    let currentPos = 0;
+    while(currentPos !== c.length){
+        if(c[currentPos+2] == 0){
+            jumps += 1;
+            currentPos +=2;
+        } else {
+            currentPos +=1;
+            jumps += 1;
+        }
+    }
+    return jumps-1;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const c = readLine().replace(/\s+$/g, '').split(' ').map(cTemp => parseInt(cTemp, 10));
+
+    const result = jumpingOnClouds(c);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
+
